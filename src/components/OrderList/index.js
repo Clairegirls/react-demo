e87dd9585272c024054d549966fc20153af40937
fanss <shanshan.fan@maitang001.com>
2020-09-08 12:22:56 +0800

    订单列表mock方式数据填充

--- src/components/OrderList/index.js
+++ src/components/OrderList/index.js
@@ -3,47 +3,33 @@ import React, {Component} from 'react';
 import OrderItem from "../OrderItem";
 import './style.css'
 
-const data = [
-    {
-        id: 1,
-        shop: '院落创意菜',
-        picture:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1087088136,954384588&fm=26&gp=0.jpg',
-        product: '西蓝花',
-        price: 19.9,
-        ifCommented: true
-    },
-    {
-        id: 2,
-        shop: '院落创意菜',
-        picture:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1087088136,954384588&fm=26&gp=0.jpg',
-        product: '西蓝花',
-        price: 19.9,
-        ifCommented: true
-    },
-    {
-        id: 3,
-        shop: '院落创意菜',
-        picture:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1087088136,954384588&fm=26&gp=0.jpg',
-        product: '西蓝花',
-        price: 19.9,
-        ifCommented: true
-    },
-    {
-        id: 4,
-        shop: '院落创意菜',
-        picture:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1087088136,954384588&fm=26&gp=0.jpg',
-        product: '西蓝花',
-        price: 19.9,
-        ifCommented: true
-    }
-]
-
 class OrderList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('/mock/orders.json').then(res=>{
+            if(res.ok){
+                res.json().then(data=>{
+                    console.log(data)
+                    this.setState({
+                        data
+                    })
+                })
+            }
+        })
+    }
+
     render() {
         return (
             <div className='orderList'>
                 {
-                    data.map(item=>{
+                    this.state.data.map(item=>{
                         return <OrderItem key={item.id} data={item}/>
                     })
                 }
